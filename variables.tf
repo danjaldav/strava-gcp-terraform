e0e9e963d15902b8eee79a160f4a1e0f060473e0
drda@telenor.dk <DRDA@telenor.dk>
2022-05-14 23:04:50 +0200

    Update variables.tf
    Variables updated

--- variables.tf
+++ variables.tf
@@ -1,4 +1,7 @@
-variable "project" {}
+variable "project" {
+  default = "strava-349420"
+}
+
 variable "region" {
   default = "us-central1"
 }
@@ -7,18 +10,27 @@ variable "package" {
   default = "strava"
 }
 
-variable "bigquery_table" {}
-variable "bucket_name" {}
+variable "bigquery_table" {
+  default = "quantified_self_strava_tbl"
+}
+
+variable "bucket_name" {
+  default = "strava-bucket"
+}
 
 # For the Webhook application
 variable "webhook_repo_url" {
   type = string
-
 }
 variable "verify_token" {
   type = string
   description = "A random string which the Webhook application uses to create your subscription."
 }
 
-variable "pull_repo_url" {}
-variable "gcs_to_gcp_repo_url" {}
+variable "pull_repo_url" {
+  default = "danjaldav"
+} 
+
+variable "gcs_to_gcp_repo_url" {
+  default = "danjaldav"
+}
